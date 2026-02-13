e041963a0f87a4d6cb26459cbec7a6e972d67deb
protoloner1 <104278458+protoloner1@users.noreply.github.com>
2026-02-13 20:39:49 +0530

    Commit

---
+++ message.js
@@ -0,0 +1,9 @@
+// message.js
+const confessionHTML = `
+    <h2>Yayyyyyy!!! 🥰💖✨</h2>
+    <p>Meri pyaari bubu... I am officially proposing to you now. The last one year with you has been the best year of my life.</p>
+    <p>I've changed a lot, transformed a lot... Just because of you. You are my life. You are my love. You are my world... And beyond.</p>
+    <p>You are my valentine. Always and forever. 💞</p>
+    <p>I love you my baby , today .. tomorrow... and till eternity</p>    
+    <p class="big-heart">❤️❤️❤️</p>
+`;
